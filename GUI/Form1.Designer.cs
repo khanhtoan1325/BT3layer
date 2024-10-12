ef750b3247bd3e45a9811512968f7caba3fc09d9
KhanhToan <nguyenkhanhtoan1325@gmail.com>
2024-10-13 00:35:10 +0700

    #Sửa Lần 1

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -294,6 +294,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(1196, 535);
             this.Controls.Add(this.chkChuaDK);
             this.Controls.Add(this.label1);
